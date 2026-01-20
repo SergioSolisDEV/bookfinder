a3fa0a4fb4c85e987f1f6fcfd256ee054199dd16
Sergio <sergiosolis1455@gmail.com>
2026-01-20 16:44:39 +0100

    Fixing helmet bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
